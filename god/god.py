5d4cf7c0b9a008fc80c9c5dd04c10dbbc68da059
eltrebolpro <117539790+eltrebolpro@users.noreply.github.com>
2024-10-23 16:19:56 +0200

    Update god.py

--- god/god.py
+++ god/god.py
@@ -17,14 +17,14 @@ class God(commands.Cog):
     @commands.guild_only()
     @commands.has_permissions(ban_members=True)
     @commands.hybrid_group(name="hakai", aliases=["hakai"])
-    async def ban(ctx, member: discord.Member, *, reason=None):
-    if self.has_higher_role(ctx, member):
-        try:
-            await ctx.send('https://tenor.com/view/beerus-gif-20373634')
-        except discord.HTTPException as error:
-            await ctx.send(f'Error al enviar el gif: {error}')
-        await asyncio.sleep(5)
-        await member.ban(reason=f'Baneado por {ctx.author}')
-        await ctx.send(f'{member.mention} ha sido baneado por {ctx.author}.')
-    else:
-        await ctx.send('No puedes banear a alguien con un rol igual o superior al tuyo.')
+    async def ban(self, ctx: commands.Context, member: discord.Member, *, reason=None):
+        if self.has_higher_role(ctx, member):
+            try:
+                await ctx.send('https://tenor.com/view/beerus-gif-20373634')
+            except discord.HTTPException as error:
+                await ctx.send(f'Error al enviar el gif: {error}')
+            await asyncio.sleep(5)
+            await member.ban(reason=f'Baneado por {ctx.author}')
+            await ctx.send(f'{member.mention} ha sido baneado por {ctx.author}.')
+        else:
+            await ctx.send('No puedes banear a alguien con un rol igual o superior al tuyo.')
